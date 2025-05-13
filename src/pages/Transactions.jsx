3abb40c84f3bb271518a9e63ab767183baef65d1
mukhriddin03 <imuhriddin700@gmail.com>
2025-05-13 17:15:10 +0500

    Transactions changed

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Transactions = () => {
   return (
-    <div>Transactions</div>
-    
+    <div>
+      <h1>kjsfngjfjgsf</h1>
+      <h1>kesbgbkfkdnjd</h1>
+    </div>
   )
 }
 
